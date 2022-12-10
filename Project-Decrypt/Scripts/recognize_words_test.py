407bc056686b03a53342970712d26b1d5106450c
Ivan Nieto <ivang9913@gmail.com>
2022-12-10 17:45:08 -0500

    P-Decrypt: Complete script to test the function recognzing texts

--- Project-Decrypt/Scripts/recognize_words_test.py
+++ Project-Decrypt/Scripts/recognize_words_test.py
@@ -6,13 +6,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import random
+import pandas as pd
 
 # ---------------------------------------------------
 # Create the real texts field with web scrap
 # ---------------------------------------------------
 
 # Set the webdriver
-executable_path = # add the path of the chromedriver as str 
+executable_path = 'C:\\Users\\ivang\\OneDrive\\Documentos\\Ciencia de datos\\chromedriver.exe'
 service = Service(executable_path)
 options = Options()
 options.headless = True
@@ -47,9 +49,16 @@ def affine_taransformation_encypt(a, b, text, tabs):
 	# a and b are the paramethers on the transformation
 	# text is the objecto encrypt 
 	# tabs the length of each block of letters on the encrypt text
+	# If tabs is 'random' then the systen will assign the spaces randomly between 1 and 9
 
 	text = text.replace(' ','').lower() # Drop the spaces and the capital letters on the text
 	encypt_text = '' # Create the variable to save the text encrypt 
+
+	if tabs == 'random':
+		key = True 
+		tabs = random.randint(1,9)
+	else:
+		key = False
 
 	cont_tabs = 0 # Variable to cont each block of the text
 
@@ -66,4 +75,83 @@ def affine_taransformation_encypt(a, b, text, tabs):
 				encypt_text += ' '
 				cont_tabs = 0
 
+				if key:
+					tabs = random.randint(1,9)
+
+
 	return encypt_text
+
+a, b = 5, 8
+
+affine_field = [affine_taransformation_encypt(a, b, x, 'random') for x in paragraphs] # Encrypt each text
+
+# ---------------------------------------------------
+# Create fake texts using letters exchange
+# ---------------------------------------------------
+
+# Define a function to exchange the letters with a dictionary having as keys the letters
+# and as values the letter to assign.
+
+def exchange_letters(dict_exchange, text, tabs):
+
+	text = text.replace(' ','').lower()
+
+	encypt_text = ''
+
+	if tabs == 'random':
+		key = True 
+		tabs = random.randint(1,9)
+	else:
+		key = False
+
+	cont_tabs = 0
+
+	for i in text:
+		if i in dict_exchange:
+			encypt_text += dict_exchange[i]
+			cont_tabs += 1
+
+			if cont_tabs == tabs:
+				encypt_text += ' '
+				cont_tabs = 0
+
+				if key:
+					tabs = random.randint(1,9) 
+
+
+	return encypt_text		
+
+Keys = list(set([chr(x) for x in range(97, 123)])) # Generate a random sort on the alphabet letters
+dict_exchange = {chr(x + 97) : Keys[x] for x in range(26)}
+
+exchange_field = [exchange_letters(dict_exchange, x, 'random') for x in paragraphs] # Encrip each text
+
+# ---------------------------------------------------
+# Create fake texts using random values
+# ---------------------------------------------------
+
+# Define a function to create a text with length given
+def random_text(len_t):
+	created_text = ''
+
+	for i in range(len_t):
+		
+		# Add a tab or letter depending of the obtained number
+		ch = random.randint(97, 123) 
+		if ch == 123:
+			created_text += ' '
+		else:
+			created_text += chr(ch)
+
+	return created_text 
+
+random_text_field = [random_text(random.randint(90, 120)) for j in paragraphs]
+
+df_dict = {'Real text' : paragraphs,
+			'Affine encrypt' : affine_field,
+			'Exchange encrypt' : exchange_field,
+			'random_text' : random_text_field}
+
+
+df = pd.DataFrame(df_dict)
+df.to_csv('Test_recognize_words.csv')
